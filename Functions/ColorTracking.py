60629a3f234e3b2e98763086024b67ddf97ea4e1
ryanschofield9 <schofier@oregonstate.edu>
2024-02-23 09:14:12 -0800

    adding perception code

--- Functions/ColorTracking.py
+++ Functions/ColorTracking.py
@@ -27,7 +27,7 @@ range_rgb = {
 }
 
 __target_color = ('red',)
-# 设置检测颜色
+# 设置检测颜色 #set the detection color 
 def setTargetColor(target_color):
     global __target_color
 
@@ -35,26 +35,26 @@ def setTargetColor(target_color):
     __target_color = target_color
     return (True, ())
 
-# 找出面积最大的轮廓
-# 参数为要比较的轮廓的列表
+# 找出面积最大的轮廓 #Find the profile with the largest area 
+# 参数为要比较的轮廓的列表 #The parameter is a list of profiles to be compared
 def getAreaMaxContour(contours):
     contour_area_temp = 0
     contour_area_max = 0
     area_max_contour = None
 
-    for c in contours:  # 历遍所有轮廓
-        contour_area_temp = math.fabs(cv2.contourArea(c))  # 计算轮廓面积
+    for c in contours:  # 历遍所有轮廓 #Go through all the outlines 
+        contour_area_temp = math.fabs(cv2.contourArea(c))  # 计算轮廓面积 #Calculate the profile area 
         if contour_area_temp > contour_area_max:
             contour_area_max = contour_area_temp
             if contour_area_temp > 300:  # 只有在面积大于300时，最大面积的轮廓才是有效的，以过滤干扰
                 area_max_contour = c
 
-    return area_max_contour, contour_area_max  # 返回最大的轮廓
+    return area_max_contour, contour_area_max  # 返回最大的轮廓 #The profile of the largest area is only valid if the area is greater than 300 to filter out interference 
 
-# 夹持器夹取时闭合的角度
+# 夹持器夹取时闭合的角度 # The angle at whilch the gripper is closed when it is gripped 
 servo1 = 500
 
-# 初始位置
+# 初始位置 # Initial position 
 def initMove():
     Board.setBusServoPulse(1, servo1 - 50, 300)
     Board.setBusServoPulse(2, 500, 500)
@@ -66,7 +66,7 @@ def setBuzzer(timer):
     time.sleep(timer)
     Board.setBuzzer(0)
 
-#设置扩展板的RGB灯颜色使其跟要追踪的颜色一致
+#设置扩展板的RGB灯颜色使其跟要追踪的颜色一致 #....... The RGB light color matches the color you want to track
 def set_rgb(color):
     if color == "red":
         Board.RGB.setPixelColor(0, Board.PixelColor(255, 0, 0))
@@ -96,7 +96,7 @@ detect_color = 'None'
 action_finish = True
 start_pick_up = False
 start_count_t1 = True
-# 变量重置
+# 变量重置 #Variabale reset
 def reset():
     global count
     global track
@@ -123,19 +123,19 @@ def reset():
     start_pick_up = False
     start_count_t1 = True
 
-# app初始化调用
+# app初始化调用 #app initialization call 
 def init():
     print("ColorTracking Init")
     initMove()
 
-# app开始玩法调用
+# app开始玩法调用 #The app starts to play the game 
 def start():
     global __isRunning
     reset()
     __isRunning = True
     print("ColorTracking Start")
 
-# app停止玩法调用
+# app停止玩法调用 #The app stops the gameplay call 
 def stop():
     global _stop 
     global __isRunning
@@ -143,7 +143,7 @@ def stop():
     __isRunning = False
     print("ColorTracking Stop")
 
-# app退出玩法调用
+# app退出玩法调用 #The app exits the gameplay call 
 def exit():
     global _stop
     global __isRunning
@@ -157,7 +157,7 @@ rotation_angle = 0
 unreachable = False
 world_X, world_Y = 0, 0
 world_x, world_y = 0, 0
-# 机械臂移动线程
+# 机械臂移动线程 #The robotic arm moves the thread 
 def move():
     global rect
     global track
@@ -173,7 +173,7 @@ def move():
     global center_list, count
     global start_pick_up, first_move
 
-    # 不同颜色木快放置坐标(x, y, z)
+    # 不同颜色木快放置坐标(x, y, z) #Coordinates of different colors of wood (x,y,z)
     coordinate = {
         'red':   (-15 + 0.5, 12 - 0.5, 1.5),
         'green': (-15 + 0.5, 6 - 0.5,  1.5),
@@ -181,56 +181,56 @@ def move():
     }
     while True:
         if __isRunning:
-            if first_move and start_pick_up: # 当首次检测到物体时               
+            if first_move and start_pick_up: # 当首次检测到物体时 #When an object is detected for the first time        
                 action_finish = False
                 set_rgb(detect_color)
                 setBuzzer(0.1)               
-                result = AK.setPitchRangeMoving((world_X, world_Y - 2, 5), -90, -90, 0) # 不填运行时间参数，自适应运行时间
+                result = AK.setPitchRangeMoving((world_X, world_Y - 2, 5), -90, -90, 0) # 不填运行时间参数，自适应运行时间 #Leave the runtime parameter blank and the runtime is adaptive
                 if result == False:
                     unreachable = True
                 else:
                     unreachable = False
-                time.sleep(result[2]/1000) # 返回参数的第三项为时间
+                time.sleep(result[2]/1000) # 返回参数的第三项为时间 #The third item of the return parameter is time 
                 start_pick_up = False
                 first_move = False
                 action_finish = True
-            elif not first_move and not unreachable: # 不是第一次检测到物体
+            elif not first_move and not unreachable: # 不是第一次检测到物体 #This is not the first time an object has been detected 
                 set_rgb(detect_color)
-                if track: # 如果是跟踪阶段
-                    if not __isRunning: # 停止以及退出标志位检测
+                if track: # 如果是跟踪阶段 #if it's a tracking phase 
+                    if not __isRunning: # 停止以及退出标志位检测 #Stop and exit flag detection 
                         continue
                     AK.setPitchRangeMoving((world_x, world_y - 2, 5), -90, -90, 0, 20)
                     time.sleep(0.02)                    
                     track = False
-                if start_pick_up: #如果物体没有移动一段时间，开始夹取
+                if start_pick_up: #如果物体没有移动一段时间，开始夹取 #.... start the gripping
                     action_finish = False
-                    if not __isRunning: # 停止以及退出标志位检测
+                    if not __isRunning: # 停止以及退出标志位检测 #Stop and exit flag detection
                         continue
-                    Board.setBusServoPulse(1, servo1 - 280, 500)  # 爪子张开
-                    # 计算夹持器需要旋转的角度
+                    Board.setBusServoPulse(1, servo1 - 280, 500)  # 爪子张开 Claws open 
+                    # 计算夹持器需要旋转的角度 #Calculate the angle at which the gripper needs to be rotated 
                     servo2_angle = getAngle(world_X, world_Y, rotation_angle)
                     Board.setBusServoPulse(2, servo2_angle, 500)
                     time.sleep(0.8)
                     
                     if not __isRunning:
                         continue
-                    AK.setPitchRangeMoving((world_X, world_Y, 2), -90, -90, 0, 1000)  # 降低高度
+                    AK.setPitchRangeMoving((world_X, world_Y, 2), -90, -90, 0, 1000)  # 降低高度 #Lower the height
                     time.sleep(2)
                     
                     if not __isRunning:
                         continue
-                    Board.setBusServoPulse(1, servo1, 500)  # 夹持器闭合
+                    Board.setBusServoPulse(1, servo1, 500)  # 夹持器闭合 # Gripper closed 
                     time.sleep(1)
                     
                     if not __isRunning:
                         continue
                     Board.setBusServoPulse(2, 500, 500)
-                    AK.setPitchRangeMoving((world_X, world_Y, 12), -90, -90, 0, 1000)  # 机械臂抬起
+                    AK.setPitchRangeMoving((world_X, world_Y, 12), -90, -90, 0, 1000)  # 机械臂抬起 #The robotic arm is raised
                     time.sleep(1)
                     
                     if not __isRunning:
                         continue
-                    # 对不同颜色方块进行分类放置
+                    # 对不同颜色方块进行分类放置 #Categorize and place blocks of different colors 
                     result = AK.setPitchRangeMoving((coordinate[detect_color][0], coordinate[detect_color][1], 12), -90, -90, 0)   
                     time.sleep(result[2]/1000)
                     
@@ -252,7 +252,7 @@ def move():
                     
                     if not __isRunning:
                         continue
-                    Board.setBusServoPulse(1, servo1 - 200, 500)  # 爪子张开，放下物体
+                    Board.setBusServoPulse(1, servo1 - 200, 500)  # 爪子张开，放下物体 #Open your paws and put down the object 
                     time.sleep(0.8)
                     
                     if not __isRunning:
@@ -260,7 +260,7 @@ def move():
                     AK.setPitchRangeMoving((coordinate[detect_color][0], coordinate[detect_color][1], 12), -90, -90, 0, 800)
                     time.sleep(0.8)
 
-                    initMove()  # 回到初始位置
+                    initMove()  # 回到初始位置 #Return to the starting position
                     time.sleep(1.5)
 
                     detect_color = 'None'
@@ -281,7 +281,7 @@ def move():
                 time.sleep(1.5)
             time.sleep(0.01)
 
-# 运行子线程
+# 运行子线程 #Run child thread 
 th = threading.Thread(target=move)
 th.setDaemon(True)
 th.start()
@@ -317,12 +317,12 @@ def run(img):
      
     frame_resize = cv2.resize(img_copy, size, interpolation=cv2.INTER_NEAREST)
     frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
-    #如果检测到某个区域有识别到的物体，则一直检测该区域直到没有为止
+    #如果检测到某个区域有识别到的物体，则一直检测该区域直到没有为止 #..... the area is detected until there is none 
     if get_roi and start_pick_up:
         get_roi = False
         frame_gb = getMaskROI(frame_gb, roi, size)    
     
-    frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # 将图像转换到LAB空间
+    frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # 将图像转换到LAB空间 #Convert images to LAB space
     
     area_max = 0
     areaMaxContour = 0
@@ -330,20 +330,20 @@ def run(img):
         for i in color_range:
             if i in __target_color:
                 detect_color = i
-                frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])  # 对原图像和掩模进行位运算
-                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # 开运算
-                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # 闭运算
-                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # 找出轮廓
-                areaMaxContour, area_max = getAreaMaxContour(contours)  # 找出最大轮廓
-        if area_max > 2500:  # 有找到最大面积
+                frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])  # 对原图像和掩模进行位运算 # Perform bit operations on the original image and mask
+                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # 开运算 #start the operation 
+                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # 闭运算 #Closed operations
+                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # 找出轮廓 #Find the outline 
+                areaMaxContour, area_max = getAreaMaxContour(contours)  # 找出最大轮廓 #Find the maximum contour 
+        if area_max > 2500:  # 有找到最大面积 #There is the largest area found 
             rect = cv2.minAreaRect(areaMaxContour)
             box = np.int0(cv2.boxPoints(rect))
 
-            roi = getROI(box) #获取roi区域
+            roi = getROI(box) #获取roi区域 #......
             get_roi = True
 
-            img_centerx, img_centery = getCenter(rect, roi, size, square_length)  # 获取木块中心坐标
-            world_x, world_y = convertCoordinate(img_centerx, img_centery, size) #转换为现实世界坐标
+            img_centerx, img_centery = getCenter(rect, roi, size, square_length)  # 获取木块中心坐标 #Get the center coordinates of the block 
+            world_x, world_y = convertCoordinate(img_centerx, img_centery, size) #转换为现实世界坐标 # ....
             
             
             cv2.drawContours(img, [box], -1, range_rgb[detect_color], 2)
@@ -353,7 +353,7 @@ def run(img):
             last_x, last_y = world_x, world_y
             track = True
             #print(count,distance)
-            # 累计判断
+            # 累计判断 #cumulative judgement 
             if action_finish:
                 if distance < 0.3:
                     center_list.extend((world_x, world_y))
---
+++ Functions/perception.py
@@ -0,0 +1,219 @@
+#!/usr/bin/python3
+# coding=utf8
+import sys
+sys.path.append('/home/pi/ArmPi/')
+import cv2
+import time
+import Camera
+import threading
+from LABConfig import *
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
+
+if sys.version_info.major == 2:
+    print('Please run this program with python3!')
+    sys.exit(0)
+
+AK = ArmIK()
+'''
+range_rgb = {
+    'red': (0, 0, 255),
+    'blue': (255, 0, 0),
+    'green': (0, 255, 0),
+    'black': (0, 0, 0),
+    'white': (255, 255, 255),
+}
+
+__target_color = ('red',)
+# 设置检测颜色 #set the detection color 
+def setTargetColor(target_color):
+    global __target_color
+
+    #print("COLOR", target_color)
+    __target_color = target_color
+    return (True, ())
+
+# 找出面积最大的轮廓 #Find the profile with the largest area 
+# 参数为要比较的轮廓的列表 #The parameter is a list of profiles to be compared
+def getAreaMaxContour(contours):
+    contour_area_temp = 0
+    contour_area_max = 0
+    area_max_contour = None
+
+    for c in contours:  # 历遍所有轮廓 #Go through all the outlines 
+        contour_area_temp = math.fabs(cv2.contourArea(c))  # 计算轮廓面积 #Calculate the profile area 
+        if contour_area_temp > contour_area_max:
+            contour_area_max = contour_area_temp
+            if contour_area_temp > 300:  # 只有在面积大于300时，最大面积的轮廓才是有效的，以过滤干扰
+                area_max_contour = c
+
+    return area_max_contour, contour_area_max  # 返回最大的轮廓 #The profile of the largest area is only valid if the area is greater than 300 to filter out interference 
+
+def reset():
+    global count
+    global track
+    global _stop
+    global get_roi
+    global first_move
+    global center_list
+    global __isRunning
+    global detect_color
+    global action_finish
+    global start_pick_up
+    global __target_color
+    global start_count_t1
+    
+    count = 0
+    _stop = False
+    track = False
+    get_roi = False
+    center_list = []
+    first_move = True
+    __target_color = ()
+    detect_color = 'None'
+    action_finish = True
+    start_pick_up = False
+    start_count_t1 = True
+
+for i in color_range:
+    if i in __target_color:
+                    detect_color = i
+                    frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])  # 对原图像和掩模进行位运算 # Perform bit operations on the original image and mask
+                    opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # 开运算 #start the operation 
+                    closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # 闭运算 #Closed operations
+                    contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # 找出轮廓 #Find the outline 
+                    areaMaxContour, area_max = getAreaMaxContour(contours)  # 找出最大轮廓 #Find the maximum contour 
+if area_max > 2500:  # 有找到最大面积 #There is the largest area found 
+            rect = cv2.minAreaRect(areaMaxContour)
+            box = np.int0(cv2.boxPoints(rect))
+
+            roi = getROI(box) #获取roi区域 #......
+            get_roi = True
+
+            img_centerx, img_centery = getCenter(rect, roi, size, square_length)  # 获取木块中心坐标 #Get the center coordinates of the block 
+            world_x, world_y = convertCoordinate(img_centerx, img_centery, size) #转换为现实世界坐标 # ....
+            
+            
+            cv2.drawContours(img, [box], -1, range_rgb[detect_color], 2)
+            cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, range_rgb[detect_color], 1) #绘制中心点
+            track = True
+
+'''
+class Perception():
+    def __init__(self):
+        self.count = 0
+        self.stop = False
+        self.square_length = 3
+        self.roi = ()
+        self.track = False
+        self.get_roi = False
+        self.center_list = []
+        self.first_move = True
+        self.target_color = ()
+        self.detect_color = 'None'
+        self.action_finish = True
+        self.start_pick_up = False
+        self.start_count_t1 = True
+        self.size = (640, 480)
+        self.last_x = 0
+        self.last_y = 0
+        self.range_rgb = {
+            'red': (0, 0, 255),
+            'blue': (255, 0, 0),
+            'green': (0, 255, 0),
+            'black': (0, 0, 0),
+            'white': (255, 255, 255),
+        }
+        self.color_range = {
+            'red': [(0, 151, 100), (255, 255, 255)], 
+            'green': [(0, 0, 0), (255, 115, 255)], 
+            'blue': [(0, 0, 0), (255, 255, 110)], 
+            'black': [(0, 0, 0), (56, 255, 255)], 
+            'white': [(193, 0, 0), (255, 250, 255)], 
+        }
+
+    def set_target_color(self,color): #color is a value of 'red', 'blue', 'green', 'black' or 'white
+        self.target_color = color 
+        return self.target_color 
+    
+    def get_frame_LAB(self,img):
+        img_copy = img.copy()
+        img_h, img_w = img.shape[:2]
+        cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
+        cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
+        frame_resize = cv2.resize(img_copy, self.size, interpolation=cv2.INTER_NEAREST)
+        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)#..... the area is detected until there is none 
+        if self.get_roi and self.start_pick_up:
+            self.get_roi = False
+            frame_gb = getMaskROI(frame_gb, self.roi, self.size)    
+        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # 将图像转换到LAB空间 #Convert images to LAB space
+        return frame_lab
+
+    def find_contours (self, frame_lab):
+
+        for i in self.color_range:
+            if i in self.target_color:
+                detect_color = i
+                frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])  # 对原图像和掩模进行位运算 # Perform bit operations on the original image and mask
+                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # 开运算 #start the operation 
+                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # 闭运算 #Closed operations
+                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # 找出轮廓 #Find the outline 
+        return contours 
+    
+    def calculate_profile_area(self, contours):
+        contour_area_holder = 0
+        max_area = 0
+        max_contour = None
+
+        for c in contours:  # 历遍所有轮廓 #Go through all the outlines 
+            contour_area_holder = math.fabs(cv2.contourArea(c))  # 计算轮廓面积 #Calculate the profile area 
+            if contour_area_holder > area_max:
+                max_area = contour_area_holder
+                if contour_area_holder > 300:  # 只有在面积大于300时，最大面积的轮廓才是有效的，以过滤干扰
+                    max_contour = c
+        return max_contour, max_area  # 返回最大的轮廓 #The profile of the largest area is only valid if the area is greater than 300 to filter out interference 
+    
+    def turn_into_box(self, max_contour):
+        rect = cv2.minAreaRect(max_contour)
+        box = np.int0(cv2.boxPoints(rect))
+        return rect, box 
+    
+    def get_ROI(self):
+        self.roi = getROI(box) #获取roi区域 #......
+        self.get_roi = True
+    
+    def get_center(self, rect):
+        img_center_x, img_center_y = getCenter(rect, self.roi, self.size, self.square_length)
+        return img_center_x, img_center_y
+
+    def get_in_world_frame(self, img_center_x, img_center_y):
+        world_x, world_y = convertCoordinate(img_center_x, img_center_y, self.size)
+        return world_x, world_y
+    
+    def draw_box(self,img, target_color, world_x, world_y):
+        cv2.drawContours(img, [box], -1, self.range_rgb[target_color], 2)
+        cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.range_rgb[target_color], 1) 
+        self.track = True
+
+if __name__ == '__main__':
+    perception = Perception()
+    target_color = perception.set_target_color('red')
+    my_camera = Camera.Camera()
+    my_camera.camera_open()
+    while True:
+        img = my_camera.frame
+        if img is not None:
+            frame = img.copy()
+            frame_lab = perception.get_frame_LAB(frame)
+            contours = perception.find_contours(frame_lab)
+            max_contour, max_area = perception.calculate_profile_area(contours)
+            rect,box = perception.turn_into_box(max_contour)
+            perception.get_ROI()
+            img_center_x, img_center_y = perception.get_center(rect)
+            world_x, world_y = perception.get_in_world_frame(img_center_x, img_center_y)
+            perception.draw_box(img,target_color, world_x, world_y)
+
